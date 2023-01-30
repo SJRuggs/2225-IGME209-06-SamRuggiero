033a4c82c3e6e63c41fdb4e173e28160ad1306e6
Sam Ruggiero <sjruggiero03@gmail.com>
2023-01-29 21:50:49 -0500

    PE3 done

---
+++ PE3 - Functions/PE3 - Functions/PE3 - Functions.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main()
+{
+	PrintName();
+}
+
+void PrintName()
+{
+	cout << "Sam Ruggiero" << endl;
+}
